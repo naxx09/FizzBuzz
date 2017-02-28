1980f82a5044396824ad0937c35c1b7c4a165b88
Jiashu Yang <naxx09@gmail.com>
2017-02-27 20:07:09 -0500

    1.0

---
+++ Fizzbuzz/Fizzbuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                if(i % 10 == 0) {
+                    Console.WriteLine();
+                        }
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("\tFizzBuzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.Write("\tFizz");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.Write("\tBuzz");
+                }
+                else
+                {
+                    Console.Write("\t{0}", i);
+                }
+            }
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
